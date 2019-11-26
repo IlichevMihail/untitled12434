944321c51ead14f9211732d69ab5865786ffcda2
Mike-Like11 <Lizasabrash2001@mail.ru>
2019-11-26 13:52:50 +0300

    Initial commit

---
+++ src/com/company/AnimalTransport.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public interface AnimalTransport {
+    int NOGA = 4;
+    void say();
+    void canSwim();
+    void canRun();
+    void CanFly();
+}
---
+++ src/com/company/Cow.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Cow implements AnimalTransport {
+    @Override
+    public void say() {
+
+    }
+
+    @Override
+    public void canSwim() {
+
+    }
+
+    @Override
+    public void canRun() {
+
+    }
+
+    @Override
+    public void CanFly() {
+
+    }
+}
